e87447cea503c2ad92f713ad15b348a08f6539ef
Jose Luis Palacios <hola@jlpalacios.es>
2022-11-19 19:59:37 +0100

    19112022b

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Content from './Content';
+import Header from './Header';
+import Total from './Total';
+
+const Course = ({courses}) => {
+    
+    return (
+        <>
+            {courses.map(course => (
+                <>
+               <Header course={course} />
+            <Content course={course} />
+            <Total course={course} /> 
+            </>
+            ))}
+            
+        </>
+    );
+}
+
+export default Course;
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Total = ({course}) => {
     let total = 0;
     course.parts.map(part => total+= part.exercises)
+
     return (
         <div>
-            <p>Number of exercises {total}</p>
+            <b>Number of exercises {total}</b>
         </div>
     );
 }
